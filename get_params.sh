8c564769bc71901e8ae1b3074118ae86e22e4f8b
jpmpentwater <millerjp@stanford.edu>
2015-08-20 15:38:21 -0700

    Download params in smaller chunks

--- get_params.sh
+++ get_params.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
+
 mkdir params
 cd params
-curl -L -o params.zip https://www.dropbox.com/sh/6wwryuc1zouxajp/AAC7FjQJes2FqcRQhmz-udT8a?dl=1
-unzip params.zip
-rm params.zip
+
+# download wordnet parameters
+mkdir wordnet
+cd wordnet
+curl -L -o wordnet.zip https://www.dropbox.com/sh/jonav59hcaoy82a/AAAhPs3i1DzbZF_kao-UrVyNa?dl=1
+unzip wordnet.zip
+rm wordnet.zip
 cd ../
+
+# download freebase parameters (one by one due to dropbox file size limits)
+mkdir freebase
+cd freebase
+mkdir bilinear
+mkdir bilinear_diag
+mkdir transE
+
+cd bilinear
+curl -L -o bilinear.zip https://www.dropbox.com/sh/fo5u9jmvhw648gr/AABtc2sHiCZasc2mQDpuHXVUa?dl=1
+unzip bilinear.zip
+rm bilinear.zip
+
+cd ../bilinear_diag
+curl -L -o bilinear_diag.zip https://www.dropbox.com/sh/r3dis9sfxufsdf3/AAAe3wPZw4GusWmm-b1FMEjwa?dl=1
+unzip bilinear_diag.zip
+rm bilinear_diag.zip
+
+cd ../transE
+curl -L -o transE.zip https://www.dropbox.com/sh/hc3ww77rp06a1x9/AACtXmhXHaln3i1gneYgVomTa?dl=1
+unzip transE.zip
+rm transE.zip
+cd ../../../
